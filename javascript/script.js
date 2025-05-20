0f071205b89704a6f9d7bdd49875c80afac3515c
johngiangrossi <john.giangrossi@email.com>
2025-05-20 22:31:08 +0200

    first commit

---
+++ javascript/script.js
@@ -0,0 +1,13 @@
+console.log('JS biglietto treno form');
+
+/*
+Il programma dovrà mostrare una form da compilare usando i corretti campi HTML di input con cui chiedere all’utente il numero di chilometri che vuole percorrere e lo sconto da applicare scegliendo tra 3 opzioni:
+prezzo intero
+sconto studente
+sconto over 65
+Sulla base di queste informazioni dovrà calcolare il prezzo totale del viaggio, secondo queste regole:
+il prezzo del biglietto è definito in base ai km (0.21 € al km)
+va applicato uno sconto del 20% per i minorenni
+va applicato uno sconto del 40% per gli over 65.
+Il recap dei dati e l’output del prezzo finale va stampato in pagina (formattato con massimo due decimali, per indicare centesimi sul prezzo).
+*/
