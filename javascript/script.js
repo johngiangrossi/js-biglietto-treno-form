035fb648ebe1e712312b87a70f1cea43728678e3
johngiangrossi <john.giangrossi@email.com>
2025-05-21 17:05:43 +0200

    ADD control if isNan

--- javascript/script.js
+++ javascript/script.js
@@ -15,18 +15,19 @@ Il recap dei dati e l’output del prezzo finale va stampato in pagina (formatta
 //prendo gli elementi dal DOM
 const inputDistance = document.getElementById('distance');
 const inputDiscount = document.getElementById('discount');
-const btnLoader = document.getElementById('load');
+const btnLoader = document.getElementById('count');
 const divRecapPrice = document.getElementById('recap-price')
 const divRecapDiscount = document.getElementById('recap-discount')
 const divFinalPrice = document.getElementById('final-price')
 
-console.log(inputDistance, inputDiscount, btnLoader, divRecapPrice, divRecapDiscount, divFinalPrice);
+// console.log(inputDistance, inputDiscount, btnLoader, divRecapPrice, divRecapDiscount, divFinalPrice);
+
 
 // funzione per lo sconto
 function getdiscount(price, discount) {
-    discountPrice = (price / 100) * discount
-    return discountPrice
-    // return number
+  discountPrice = (price / 100) * discount
+  return discountPrice
+  // return number
 }
 
 const priceForKm = 0.21
@@ -34,37 +35,51 @@ const priceForKm = 0.21
 
 //aggiungo evento listener
 btnLoader.addEventListener('click', function () {
-    console.log('calcolo prezzo');
-    
-    // prendo i valori inseriti dall'utente
-    const distance = parseFloat(inputDistance.value);
-    const discount = inputDiscount.value;
-
-    console.log(distance, discount);
-
-    // calcolo gli sconti e prezzo finale
-    priceTicket = priceForKm * distance;
-    finalDiscount = getdiscount(priceTicket, 0)
-    finalPrice = priceTicket;
-
-    if (discount === 'student-discount') {
-        finalDiscount = getdiscount(priceTicket, 20)
-        finalPrice = priceTicket - finalDiscount
-    } else if (discount === 'senior-discount') {
-        finalDiscount = getdiscount(priceTicket, 40)
-        finalPrice = priceTicket - finalDiscount
-    }
-
-    console.log(finalPrice , finalDiscount);
-
-    // stampo il risultato
+  // console.log('calcolo prezzo');
+  
+  // prendo i valori inseriti dall'utente
+  const distance = Math.abs(parseFloat(inputDistance.value));
+  const discount = inputDiscount.value;
+  
+  // verifico utente inserisca valori validi
+  if (isNaN(distance)) {
     divRecapPrice.innerHTML = `${new Intl.NumberFormat("it-IT", { style: "currency", currency: "EUR" }).format(
-        priceTicket,
-      )}`
-    divRecapDiscount.innerHTML = `${new Intl.NumberFormat("it-IT", { style: "currency", currency: "EUR" }).format(
-        finalDiscount,
-      )}`  
-    divFinalPrice.innerHTML = `${new Intl.NumberFormat("it-IT", { style: "currency", currency: "EUR" }).format(
-        finalPrice,
-      )}`
+      0,
+    )}`
+  divRecapDiscount.innerHTML = `${new Intl.NumberFormat("it-IT", { style: "currency", currency: "EUR" }).format(
+      0,
+    )}`  
+  divFinalPrice.innerHTML = `${new Intl.NumberFormat("it-IT", { style: "currency", currency: "EUR" }).format(
+      0,
+    )}`
+    return alert('inserire un numero valido')
+  }
+
+  // console.log(distance, discount);
+
+  // calcolo gli sconti e prezzo finale
+  priceTicket = priceForKm * distance;
+  finalDiscount = getdiscount(priceTicket, 0)
+  finalPrice = priceTicket;
+
+  if (discount === 'student-discount') {
+      finalDiscount = getdiscount(priceTicket, 20)
+      finalPrice = priceTicket - finalDiscount
+  } else if (discount === 'senior-discount') {
+      finalDiscount = getdiscount(priceTicket, 40)
+      finalPrice = priceTicket - finalDiscount
+  }
+
+  // console.log(finalPrice , finalDiscount);
+
+  // stampo il risultato
+  divRecapPrice.innerHTML = `${new Intl.NumberFormat("it-IT", { style: "currency", currency: "EUR" }).format(
+      priceTicket,
+    )}`
+  divRecapDiscount.innerHTML = `${new Intl.NumberFormat("it-IT", { style: "currency", currency: "EUR" }).format(
+      finalDiscount,
+    )}`  
+  divFinalPrice.innerHTML = `${new Intl.NumberFormat("it-IT", { style: "currency", currency: "EUR" }).format(
+      finalPrice,
+    )}`
 })
